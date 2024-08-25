1be551d4f4b8d763807297b4427feaa7c7f2a129
Fernanda Ramirez <nandapop@gmail.com>
2024-08-25 21:44:04 +0200

    Exploratory data analysis (EDA)

--- src/app.py
+++ src/app.py
@@ -1,4 +1,53 @@
-from utils import db_connect
-engine = db_connect()
+'''I didn't create model classes as suggested by the README file.
 
-# your code here
+`models/` - This directory should contain your SQLAlchemy model classes.
+'''
+from utils import *
+from sqlalchemy import create_engine
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_parameters = {
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
+}
+
+def show_data(cursor):
+    select_query = (
+            """
+            SELECT * FROM test LIMIT 10; 
+            """,
+            """
+            SELECT * FROM  train LIMIT 10;
+        """)
+    for query in select_query:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        print(f"\nResults for query",query)
+        for row in rows:
+            print(row)
+
+if __name__ == '__main__':
+    
+    my_connection = psycopg2.connect(
+        host = db_parameters['host'],
+        database = db_parameters['database'], 
+        user = db_parameters['user'],
+        password = db_parameters['password']
+    )   
+cursor = my_connection.cursor()
+
+create_tables()
+csv_files = {
+        'test': '../machine-learning-python-template/data/processed/clean_airbnb_test.csv',
+        'train': '../machine-learning-python-template/data/processed/clean_airbnb_train.csv'
+    }
+load_files(csv_files)
+show_data(cursor)            
+cursor.close()
+my_connection.close()
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,78 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
+''''#psql -h localhost -U airbnb_ny_user -d airbnb_ny
+#dropdb -h localhost -U postgres sample-db
+#\l list databases
+#\dt list all tables'''
 import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+import os
+from dotenv import load_dotenv
 
-# load the .env file variables
 load_dotenv()
 
+db_parameters = {
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
+}
+engine = create_engine(os.getenv('DATABASE_URL'))
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+
+if engine:
+    print("connected")
+
+def delete_tables(cursor):
+    
+    drop_queries = (
+        "DROP TABLE IF EXISTS test;",
+        "DROP TABLE IF EXISTS train;"
+    )
+    
+    for query in drop_queries:
+        cursor.execute(query)
+
+    
+def create_tables():
+    my_connection = psycopg2.connect(
+    host = db_parameters['host'],
+    database = db_parameters['database'], 
+    user = db_parameters['user'],
+    password = db_parameters['password']
+    )   
+    cursor = my_connection.cursor()
+    delete_tables(cursor)
+    
+    queries = (
+            """
+            CREATE TABLE IF NOT EXISTS test (
+                id SERIAL PRIMARY KEY,
+                minimum_nights INTEGER,
+                number_of_reviews INTEGER,
+                reviews_per_month INTEGER,
+                price   FLOAT
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS train (
+                id SERIAL PRIMARY KEY,
+                column1 VARCHAR(255),
+                column2 INTEGER,
+                column3 DATE
+            )
+        """)
+    for query in queries:
+        cursor.execute(query)
+        
+    my_connection.commit()
+    print("Tables created.")
+    cursor.close()
+    my_connection.close()
+    
+if __name__ == '__main__':
+    create_tables()
+
+def load_files(csv_files):
+    for table_name, file_path in csv_files.items():
+        df = pd.read_csv(file_path)
+        df.to_sql(table_name, engine, if_exists='replace', index=False)  
